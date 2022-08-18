c31b11281d325061b2f80a2a587ac58d05427bb6
Fedar <103038440+Graf-12@users.noreply.github.com>
2022-08-18 16:36:46 +0300

    Create aWolfInSheep'sClothing.js

---
+++ JS/aWolfInSheep'sClothing.js
@@ -0,0 +1,7 @@
+function warnTheSheep(queue) {
+    if (queue[queue.length - 1] === 'wolf') {
+        return "Pls go away and stop eating my sheep"
+    } else {
+        return `Oi! Sheep number ${queue.reverse().indexOf('wolf')}! You are about to be eaten by a wolf!`
+    }
+}
